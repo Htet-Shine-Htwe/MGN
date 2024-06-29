6c9faaff06489162b7a7bbab7c6fe16769115833
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-06-29 22:46:56 +0630

    [develop]add data client

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -23,5 +23,19 @@ class AppServiceProvider extends ServiceProvider
 
         // DbPartitionInterface
 
+        $this->app->singleton('MangaTestClient', function () {
+            $client = new \GuzzleHttp\Client([
+                'base_uri' => 'https://myanimelist.p.rapidapi.com/',
+                'http_errors' => false,
+                'headers' => [
+                    'accept' => 'application/json',
+                    'x-rapidapi-host' => 'myanimelist.p.rapidapi.com',
+                    'x-rapidapi-key' => 'bdb606d03dmshf9031f66fc37266p1aa047jsnc218d1a5aa84',
+                ]
+            ]);
+
+            return $client;
+        });
+
     }
 }
