b485efca4c76cffd35998e34d873ee05fd6f7acd
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-06-29 22:16:43 +0630

    [develop]add new mogou type

---
+++ backend/app/Models/MogouType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MogouType extends Model
+{
+    use HasFactory;
+}
