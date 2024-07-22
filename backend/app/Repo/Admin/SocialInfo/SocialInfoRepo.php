6bf4fc27dcad8d25ff14a36e92395e09d5b76a22
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-07-22 13:07:39 +0630

    [develop] custom rules and social info model

---
+++ backend/app/Repo/Admin/SocialInfo/SocialInfoRepo.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Repo\Admin\SocialInfo;
+
+use App\Models\SocialInfo;
+use HydraStorage\HydraStorage\Traits\HydraMedia;
+
+class SocialInfoRepo
+{
+
+    use HydraMedia;
+
+    protected $model;
+
+    public function __construct()
+    {
+        $this->model = new SocialInfo();
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function create($data)
+    {
+        if (isset($data['cover_photo'])) {
+            $data['cover_photo'] = $this->storeMedia($data['cover_photo'], 'social_info');
+        }
+
+        return $this->model->create($data);
+    }
+
+    public function update($id, $data)
+    {
+        $socialInfo = $this->model->find($id);
+
+        if (isset($data['cover_photo'])) {
+            $data['cover_photo'] = $this->storeMedia($data['cover_photo'], 'social_info');
+            $this->removeMedia(storage_path('app/public/social_info/' . $socialInfo->cover_photo));
+        }
+
+        return $socialInfo->update($data);
+    }
+
+
+}
