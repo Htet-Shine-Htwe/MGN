3b607ef494de29c15acce5a6683c94f468a2a047
Htet-Shine-Htwe <htetshine.coin@gmail.com>
2024-08-01 21:39:22 +0630

    [develop]lazy import related mogou

--- frontend/src/pages/users/Show/RelatedMogou.tsx
+++ frontend/src/pages/users/Show/RelatedMogou.tsx
@@ -1,7 +1,17 @@
 import { Card, CardContent, CardTitle } from "@/components/ui/card"
+import useQuery from "@/hooks/useQuery";
+import { Link } from "react-router-dom";
 
 
-const RelatedMogou = () => {
+interface RelatedMogouProps {
+  slug : string 
+}
+
+const RelatedMogou = ({slug} : RelatedMogouProps) => {
+
+  const { data: relatedMogous, isLoading, isFetching } = useQuery(`users/mogous/${slug}/related`);
+
+
   return (
     <Card className="w-full py-8 px-4">
         
@@ -9,9 +19,29 @@ const RelatedMogou = () => {
             You may also like
         </CardTitle>
 
-        <CardContent>
-
-        </CardContent>
+        {
+          (!isLoading ) && (
+            <CardContent 
+            className="grid grid-cols-1 gap-4 mt-8"
+            >
+              {
+                relatedMogous?.mogous?.map((mogou)=>{
+                  return (
+                    <Link
+                    to={`/show/${mogou.slug}`}
+                    key={mogou.id} className="flex items-center gap-4">
+                      <img src={mogou.cover} alt={mogou.title} className="w-20  object-cover rounded-md" />
+                      <div>
+                        <h4 className="text-md font-semibold">{mogou.title}</h4>
+                        <p className="text-sm text-muted-foreground">{mogou.author}</p>
+                      </div>
+                    </Link>
+                  )
+                })
+              }
+          </CardContent>
+          )
+        }
 
     </Card>
   )
